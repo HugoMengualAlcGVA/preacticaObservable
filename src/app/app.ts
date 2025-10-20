e169ef9023aaf7492bad393781421239004eacdf
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2025-10-20 13:37:06 +0200

    creado component1 y component2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { Component1 } from "./components/component1/component1";
+import { Component2 } from "./components/component2/component2";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Component1, Component2],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/component2/component2.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-component2',
+  imports: [],
+  templateUrl: './component2.html',
+  styleUrl: './component2.scss'
+})
+export class Component2 {  
+  mensaje:BehaviorSubject<string>=new BehaviorSubject('Valor inicial');
+  mensaje$: Observable<string> = this.mensaje.asObservable();
+
+  miSuscripcion!: Subscription;
+
+
+  subscribir(){
+    console.log('Nos suscribimos. Recibiremos todos los datos que se emitan')
+      this.miSuscripcion = this.mensaje$.subscribe({
+      next: dato => console.log(dato),
+      error: error => console.error(error),
+      complete: () => console.log('Observable completado')
+    });
+  }
+
+  emitir(){
+    console.log('Emitimos valores por el observable');
+    this.mensaje.next("Valor 1");
+    this.mensaje.next("Valor 2");
+    this.mensaje.next("Valor n");
+  }
+  
+  completar(){
+    this.mensaje.complete()
+    console.log('Observable completado. Ya no se emiten mas datos')
+  }
+
+  cancelar(){
+    this.miSuscripcion.unsubscribe();
+    console.log('Cancelacion realizada. No se reciben mas datos');
+  }
+}
