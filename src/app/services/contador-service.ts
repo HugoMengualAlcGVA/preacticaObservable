52cea50dcf89e0bc7ed1d468ed9cda0503e1705a
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2025-10-21 13:31:47 +0200

    texto arreglado

--- src/app/services/contador-service.ts
+++ src/app/services/contador-service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class ContadorService {
   private _contador:BehaviorSubject<number> = new BehaviorSubject(0);
-  contador$: Observable<number> = this._contador.asObservable();
+  public readonly contador$: Observable<number> = this._contador.asObservable();
 
     incrementar(valor:number){
         this._contador.next(valor);
